b924cbd46fc979617e75bd08c7a816c1fb5b2c98
vishvesh <kapadia.vishvesh@gmail.com>
2018-02-27 21:55:28 +0530

    2nd version

--- webapp/views.py
+++ webapp/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from .models import employees
 from .serializers import employeeSerializer
 
+
 @api_view(['GET', 'POST', ])
 def get_list(request):
     if request.method == 'GET':
@@ -16,15 +17,12 @@ def get_list(request):
         employees1 = employees.objects.all()
         serializer = employeeSerializer(employees1, many=True)
         return Response(serializer.data)
-        
 
-
-def employeedetail(request,emp_id):
+@api_view(['GET', 'POST', ])
+def employeedetail(request, emp_id):
     print("in id")
     if request.method == 'GET':
-        print("emp:-"+emp_id)
+        print("emp:-" + emp_id)
         employees1 = employees.objects.get(emp_id=emp_id)
-        serializer = employeeSerializer(employees1)
-        print(serializer.data)
+        serializer = employeeSerializer(employees1,many=False)
         return Response(serializer.data)
-
